e23d47fa429ef1531708849d0141ee88f72d3076
Samsu <biosamsuddin@gmail.com>
2023-10-17 15:10:09 +0530

    experience page finally done at office

--- app/experince/page.js
+++ app/experince/page.js
@@ -2,10 +2,84 @@
 
 
 const ExploreAtlanta=()=>{
-    return(
-        <>
-        <h1 className="text-center text-3xl leading-8 tracking-widest">THE NEIGHBORHOOD</h1>
-        </>
-    )
+    return (
+      <>
+        <section className="bg-purple-200 px-[50px] py-[50px] ">
+          <div className="max-w-[1200px] mx-auto flex-flex-col">
+            <h1 className="text-center text-3xl leading-10 tracking-widest">
+              THE NEIGHBORHOOD
+            </h1>
+            <h1 className="text-center my-4 py-4">
+              LOCAL FAVORITES AROUND THE HOTEL
+            </h1>
+            <div className="flex flex-col lg:flex-row">
+              <div className="md:w-1/2 py-4">
+                <div className="w-full h-[300px]">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="https://images.squarespace-cdn.com/content/v1/5ea0614a23978f42dee12261/1588278506047-NBDST8W9DR53J6BFPF0Y/clermont_map_02.png?format=2500w"
+                  />
+                </div>
+              </div>
+              <div className="md:w-1/2 py-4">
+                <div className="flex flex-col md:flex-row">
+                  <div class="sqs-html-content pl-4">
+                    <h3 className="">
+                      1. FELLINI’S
+                      <br />
+                      2. LA FONDA
+                      <br />
+                      3.&nbsp;LIBRARY
+                      <br />
+                      4. PUBLIX
+                      <br />
+                      5. MAJESTIC DINER
+                      <br />
+                      6. PLAZA THEATER
+                      <br />
+                      7. VIDEODROME
+                      <br />
+                      8. MIDTOWN CLEANERS AND LAUNDRY
+                      <br />
+                      9. MANUEL’S TAVERN{" "}
+                    </h3>
+                  </div>
+                  <div class="sqs-html-content pl-4">
+                    <h3 className="white-space:pre-wrap;">
+                      10.OLD FOURTH WARD SKATE PARK
+                      <br />
+                      11. INMAN PARK
+                      <br />
+                      12. JAKE’S ICE CREAM <br />
+                      13. PONCE CITY MARKET
+                      <br />
+                      14. STARBUCKS, WHOLE FOODS
+                      <br />
+                      15. RELAY BIKE CLUB
+                      <br />
+                      16. BOTANICAL GARDEN <br />
+                      17. 8ARM
+                      <br />
+                      18. GREEN’S LIQUOR STORE, KROGER
+                      <br />
+                      19. JAVA JIVE
+                    </h3>
+                    <h3 className="white-space:pre-wrap;">20. Illuminarium </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="max-w-5xl mx-auto mt-8">
+  <div className="h-[400px]">
+    <img className="w-full h-full object-contain" src="https://images.squarespace-cdn.com/content/v1/5ea0614a23978f42dee12261/1588101774128-FCX0SKYYGEMNB1ODTOW8/cle_postcard_02.jpg?format=1500w" alt="Image" />
+  </div>
+</div>
+
+            <h1 className="text-center py-4 tracking-wider">“ATLANTA IS NOT A CITY; IT IS A LANDSCAPE”</h1>
+          </div>
+        </section>
+      </>
+    );
 }
 export default ExploreAtlanta;
